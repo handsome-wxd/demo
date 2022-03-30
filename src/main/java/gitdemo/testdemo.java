6ac1c73469f2818f00480acf94d65f6f9f05f1db
wxd <1844037805@qq.com>
2022-03-30 12:36:31 +0800

    idea-git 第一次测试

---
+++ src/main/java/gitdemo/testdemo.java
@@ -0,0 +1,34 @@
+package gitdemo;
+
+public class testdemo {
+    Integer id;
+    String name;
+    public  void print()
+    {
+        System.out.println("hello git");
+    }
+    public testdemo() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public testdemo(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+}
